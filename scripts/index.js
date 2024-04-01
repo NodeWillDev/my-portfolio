fa9ac2f497c61a3710acf32f9f367ee4218f32b2
NodeWillDev <williamdasilva.dev@gmail.com>
2024-03-31 23:30:46 -0300

    In progress...

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+import pt from "./data/translate-pt.json" assert { type: "json" };
+import en from "./data/translate-en.json" assert { type: "json" };
 import "./navigation.js";
 import "./particles.js";
 import "./switch-theme.js";
+
+/**
+ * This is for tests
+ * This is for tests
+ * This is for tests
+ * This is for tests
+ * This is for tests
+ */
+
+/** @type {HTMLButtonElement} */
+const btn = document.querySelector(".ajshdjk");
+
+let ln = "pt";
+
+btn.addEventListener("click", () => {
+  /** @type {HTMLElement[]} */
+  const translate = document.querySelectorAll("._translate");
+
+  let langue = (ln = ln == "pt" ? "en" : "pt") == "en" ? en : pt;
+
+  translate.forEach((element, index) => {
+    element.innerHTML = langue[index];
+    console.log(element);
+  });
+});
