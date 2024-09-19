0ab38505d85cad13378cbbb2ff067b358239d41a
NodeWillDev <williamdasilva.dev@gmail.com>
2024-09-19 13:21:23 -0300

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // import pt from "./data/translate-pt.json" assert { type: "json" };
 // import en from "./data/translate-en.json" assert { type: "json" };
 import "./navigation.js";
-import "./particles.js";
+// import "./particles.js";
 import "./switch-theme.js";
 // import "./time-line.js";
 import "./contact.js";
